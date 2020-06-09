eba2db178ccff82f2d76bb0530ed80e48c23be9f
SaltyAom <aomkirby@mystiar.com>
2020-06-10 00:07:50 +0700

    :tada: Using Cluster mode

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const app = require("./services/app"),
-    { run } = require("./services/common")
+const cluster = require('cluster'),
+    cpu = require('os').cpus().length
+
+const app = require('./services/app'),
+    { run } = require('./services/common')
 
 app.register(require('./controllers/menu'))
 
-run(app)
+if(cluster.isMaster)
+    for(let process = 0; process < cpu; process++)
+        cluster.fork()
+else
+    run(app)
 
-if(process.env.NODE_ENV = "development")
-    module.exports = app
+if ((process.env.NODE_ENV = 'development')) module.exports = app
